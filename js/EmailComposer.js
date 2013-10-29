b3ffe6c0fced42cd4a2497c5358815ab507541cb
budgetappdev <contact@budgetappdev.com>
2013-10-29 16:37:53 +0100

    Update EmailComposer.js

--- js/EmailComposer.js
+++ js/EmailComposer.js
@@ -1,61 +1,64 @@
-// window.plugins.emailComposer
-
 function EmailComposer() {
 	this.resultCallback = null; // Function
 }
 
 EmailComposer.ComposeResultType = {
-Cancelled:0,
-Saved:1,
-Sent:2,
-Failed:3,
-NotSent:4
+	Cancelled : 0,
+	Saved : 1,
+	Sent : 2,
+	Failed : 3,
+	NotSent : 4
 }
-
-
 
 // showEmailComposer : all args optional
 
-EmailComposer.prototype.showEmailComposer = function(subject,body,toRecipients,ccRecipients,bccRecipients,bIsHTML) {
+EmailComposer.prototype.showEmailComposer = function(subject, body,
+		toRecipients, ccRecipients, bccRecipients, bIsHTML, attachments, attachmentsData) {
+	console.log("****************************AVVIATO");
 	var args = {};
-	if(toRecipients)
+	if (toRecipients)
 		args.toRecipients = toRecipients;
-	if(ccRecipients)
+	if (ccRecipients)
 		args.ccRecipients = ccRecipients;
-	if(bccRecipients)
+	if (bccRecipients)
 		args.bccRecipients = bccRecipients;
-	if(subject)
+	if (subject)
 		args.subject = subject;
-	if(body)
+	if (body)
 		args.body = body;
-	if(bIsHTML)
+	if (bIsHTML)
 		args.bIsHTML = bIsHTML;
-	
-	cordova.exec(null, null, "EmailComposer", "showEmailComposer", [args]);
+	if (attachments)
+		args.attachments = attachments;
+    if (attachmentsData)
+        args.attachmentsData = attachmentsData;
+
+	cordova.exec(null, null, "EmailComposer", "showEmailComposer", [ args ]);
 }
 
-// this will be forever known as the orch-func -jm
-EmailComposer.prototype.showEmailComposerWithCB = function(cbFunction,subject,body,toRecipients,ccRecipients,bccRecipients,bIsHTML) {
-	this.resultCallback = cbFunction;
-	this.showEmailComposer.apply(this,[subject,body,toRecipients,ccRecipients,bccRecipients,bIsHTML]);
+EmailComposer.prototype.showEmailComposerWithCallback = function(callback,
+		subject, body, toRecipients, ccRecipients, bccRecipients, isHTML,
+		attachments, attachmentsData) {
+	this.resultCallback = callback;
+	this.showEmailComposer.apply(this, [ subject, body, toRecipients,
+			ccRecipients, bccRecipients, isHTML, attachments, attachmentsData]);
 }
 
 EmailComposer.prototype._didFinishWithResult = function(res) {
 	this.resultCallback(res);
 }
 
+cordova.addConstructor(function() {
+	console.log("****************************");
+	if (!window.plugins) {
+		window.plugins = {};
+	}
 
+	// shim to work in 1.5 and 1.6
+	if (!window.Cordova) {
+		window.Cordova = cordova;
+	}
+	;
 
-cordova.addConstructor(function()  {
-					   if(!window.plugins)
-					   {
-					   window.plugins = {};
-					   }
-					   
-					   // shim to work in 1.5 and 1.6
-					   if (!window.Cordova) {
-					   window.Cordova = cordova;
-					   };
-					   
-					   window.plugins.emailComposer = new EmailComposer();
-					   });
+	window.plugins.emailComposer = new EmailComposer();
+});
